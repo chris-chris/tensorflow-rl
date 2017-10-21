0e119341e621876a67af3324b6c9bce62c372b62
Chris <sjhshy@gmail.com>
2017-10-22 01:08:47 +0900

    update

--- examples/ch2/matrix2.py
+++ examples/ch2/matrix2.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-A = np.array([[1,2,3],[4,5,6]])
+A = np.array([[1,2,3],
+              [4,5,6]])
 print("A :")
 print(A)
 
@@ -8,3 +9,13 @@ B = A.transpose()
 print("B = A.transpose()")
 print("B :")
 print(B)
+"""
+A :
+[[1 2 3]
+ [4 5 6]]
+B = A.transpose()
+B :
+[[1 4]
+ [2 5]
+ [3 6]]
+"""
